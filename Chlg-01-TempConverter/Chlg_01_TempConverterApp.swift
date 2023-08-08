ef3cbda34ec7eed3bde1d903685dfe77396b2bb4
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-08 12:21:30 +0300

    Initial Commit

---
+++ Chlg-01-TempConverter/Chlg_01_TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chlg_01_TempConverterApp.swift
+//  Chlg-01-TempConverter
+//
+//  Created by Андрей Бородкин on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chlg_01_TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
